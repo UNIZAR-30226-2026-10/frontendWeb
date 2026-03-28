38741c0c82ffe3f680d8163d0a11fd714bd16c2d
Mario-2105 <897566@unizar.es>
2026-03-28 22:53:03 +0100

    Fix diseño continuar partida

--- src/app/juego/continuarpartida/page.tsx
+++ src/app/juego/continuarpartida/page.tsx
@@ -6,14 +6,14 @@ import SlotPartida from "@/components/interfaz/SlotPartida";
 
 export default function ContinuarPartida() {
     return (
-        <main className="min-h-screen">
-            <div className="flex justify-center items-center gap-12 text-2xl ">
-                <Link href="/juego" className="flex text-white font-sans hover:text-gray-300">Crear Partida</Link>
-                <h1 className="flex underline font-sans text-gray-400 cursor-pointer">
-                    Continuar
-                </h1>
+        <main className="flex-1 w-full flex flex-col bg-transparent overflow-hidden p-4">
+            <div className="flex justify-center items-center gap-12 text-2xl mb-2 shrink-0">
+                <Link href="/juego" className="flex text-white font-sans hover:text-gray-300">
+                    Crear Partida
+                </Link>
+                <h1 className="flex underline font-sans text-gray-400 cursor-pointer">Continuar</h1>
             </div>
-            <ul className="mt-4 flex flex-col text-white w-full ">
+            <ul className="mt-4 flex flex-col text-white w-full items-center gap-4">
                 <SlotPartida jugadores={["Jugador 1", "Jugador 2"]} fechaCreacion="2024-06-01" turnoActual="Jugador 1" creadorPartida="Jugador 1"/>
                 <SlotPartida jugadores={["Jugador 1", "Jugador 2", "Jugador 3"]} fechaCreacion="2024-06-02" turnoActual="Jugador 2" creadorPartida="Jugador 2"/>
             </ul>
