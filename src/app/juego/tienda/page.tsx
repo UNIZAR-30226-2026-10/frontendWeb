29a23c3997c17195defd051bc8a7e2e5d04d5456
Alberto Duplá <898896@unizar.es>
2026-04-05 18:59:48 +0200

    [FEATURE]: Añadidos ajustes a la pantalla del lobby para el formato con conexion a la API

--- src/app/juego/tienda/page.tsx
+++ src/app/juego/tienda/page.tsx
@@ -32,7 +32,7 @@ export default function Tienda() {
           Tienda
         </h1>
         <div className="absolute right-0 w-40 h-16 bg-gray-800 rounded-2xl flex items-center justify-center text-xl shadow-md">
-          <span>Sep logo {tienda.sepDisponible}</span>
+          <span>Sep {tienda.sepDisponible}</span>
         </div>
       </div>
 
